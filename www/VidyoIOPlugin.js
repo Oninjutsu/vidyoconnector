45c568c6cc565d347a7628e7ca56bad217357d57
Oninjutsu <65342582+Oninjutsu@users.noreply.github.com>
2020-06-15 17:34:34 +0800

    Update VidyoIOPlugin.js

--- www/VidyoIOPlugin.js
+++ www/VidyoIOPlugin.js
@@ -8,4 +8,8 @@ plugin.prototype.launchVidyoIO = function(args) {
     exec(function(res){}, function(err){}, "VidyoIOPlugin", "launchVidyoIO", args);
 }
 
+VidyoIOPlugin.prototype.disconnect = function() {
+    exec(function(res){},function(err){}, "VidyoIOPlugin", "disconnect", null);
+}
+
 module.exports = new plugin();
